ac390da7e43d73c1ff6c9ba8eb7cbaf3874e00e6
PSMattingley <paul_mattingley@hotmail.com>
2021-05-10 11:24:26 +0100

    Add Dockerfile to build flask app and manual commands to run locally

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 docker build -t tf-serving -f Dockerfile-TF .
+docker build -t img-class -f Dockerfile-IC .
 docker run --name tf-serving-cont -p 8501:8501 -e MODEL_NAME=resnet_v2_fp32_savedmodel_NHWC_jpg tf-serving
+docker run --name img-class-cont -p 5000:5000 img-class
 
